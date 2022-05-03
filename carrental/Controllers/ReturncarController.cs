e948c5e04c9d928fbb9c32b259fda51586d48ba9
glpat-KDiotfmiMEnKvyyL4Dpr <glpat-KDiotfmiMEnKvyyL4Dpr>
2022-05-03 10:43:20 +0530

    Car Rental System

---
+++ carrental/Controllers/ReturncarController.cs
@@ -0,0 +1,54 @@
+﻿using carrental.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.SqlServer;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace carrental.Controllers
+{
+
+    public class ReturncarController : Controller
+    {
+        supercarEntities db = new supercarEntities();
+        // GET: Returncar
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Save(returncar recar)
+        {
+            if(ModelState.IsValid)
+            {
+                db.returncars.Add(recar);
+                var car = db.carregs.SingleOrDefault(e => e.carno == recar.carno);
+                if (car == null)
+                    return HttpNotFound("CarNo not found");
+                car.available = "Yes";
+                db.Entry(car).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(recar);
+        }
+
+        [HttpPost]
+        public ActionResult Getid(String carno)
+        {
+            var carn = (from s in db.rentails
+                        where s.carid == carno
+                        select new
+                        {
+                            StartDate = s.sdate,
+                            EndDate = s.edate,
+                            Custid = s.custid,
+                            CarNo = s.carid,
+                            Fee = s.fee,
+                            ElapsedDays = SqlFunctions.DateDiff("day", s.edate, DateTime.Now)
+                        }).ToArray();
+            return Json(carn, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ carrental/Models/RentailViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace carrental.Models
+{
+    public class RentailViewModel
+    {
+        public int id { get; set; }
+        public string carid { get; set; }
+        public Nullable<int> custid { get; set; }
+        public Nullable<int> fee { get; set; }
+        public Nullable<System.DateTime> sdate { get; set; }
+        public Nullable<System.DateTime> edate { get; set; }
+        public string available { get; set; }
+    }
+}
